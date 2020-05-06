da04c9d80450143a6a209b6f1bf7540e1031035d
KrzysiekDev <krzysztof.michalski1999@gmail.com>
2020-05-06 15:07:08 +0200

    Refactored files.py module

--- hdp/process/files.py
+++ hdp/process/files.py
@@ -1,30 +1,4 @@
-def load_csv(csv_files: list):
-    pass
-
-
-def load_txt(txt_files: list):
-    pass
-
-
-def load_xlsx(xlsx_files: list):
-    pass
-
-
-def load_xls(xls_files: list):
-    pass
-
-
-def load_gpx(gpx_files: list):
-    pass
-
-
-def load_tcx(tcx_files: list):
-    pass
-
-
-def load_jpg(jpg_files: list):
-    pass
-
+from hdp.process.load import *
 
 supported_formats = {'csv': load_csv,
                      'txt': load_txt,
---
+++ hdp/process/load.py
@@ -0,0 +1,30 @@
+def clean_up(datatable_list):
+    pass
+
+
+def load_csv(csv_files: list):
+    pass
+
+
+def load_txt(txt_files: list):
+    pass
+
+
+def load_xlsx(xlsx_files: list):
+    pass
+
+
+def load_xls(xls_files: list):
+    pass
+
+
+def load_gpx(gpx_files: list):
+    pass
+
+
+def load_tcx(tcx_files: list):
+    pass
+
+
+def load_jpg(jpg_files: list):
+    pass
