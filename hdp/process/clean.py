5c65e96214b324c470986a4a47b786ca54e0676c
KrzysiekDev <krzysztof.michalski1999@gmail.com>
2020-06-01 23:52:18 +0200

    Modified flag mode in convert_time function.

--- hdp/process/clean.py
+++ hdp/process/clean.py
@@ -11,36 +11,41 @@ units_conversions = {}
 # Team 2
 def convert_time(time: str, time_format: str = None, mode: str = 'flag'):
     """
-    Converts string with time into pd.Timedelta object
-    Parameters
-    ----------
-    time: string
-    time_format: string
-        It could contain valid flag for pd.to_datetime function - if mode = flag or
-        raw string with regex - if mode = regex, default = None
-    mode: string with value 'flag' or 'regex'
-        Flag for using flag mode (matching time using flags recognised by pd.to_datetime,
-        regex for using regex patterns matching time
-    Returns
-    -------
-    pd.Timedelta object
-        If operation was successful else returns time parameter unchanged.
-    """
+        Converts string with time into pd.Timedelta object
+
+        Parameters
+        ----------
+        time: string
+
+        time_format: string
+            It could contain valid flag for pd.to_datetime function - if mode = flag or
+            raw string with regex - if mode = regex, default = None
+
+        mode: string with value 'flag' or 'regex'
+            Flag for using flag mode (matching time using flags recognised by pd.to_datetime,
+            regex for using regex patterns matching time
+
+        Returns
+        -------
+        pd.Timedelta object
+            If operation was successful else returns time parameter unchanged.
+        """
     if mode == 'flag':
         try:
-            dt_time = pd.to_datetime(time.split(' ')[-1].split('.')[0], 'ignore', format=time_format)  # Removing date
-            # and microseconds
-            try:
-                return pd.to_timedelta(dt_time)
-            except ValueError:
-                return pd.to_timedelta(dt_time.time().__str__())
+            dt_time = pd.to_datetime("01-01-1970 " + time, 'ignore', format="%d-%m-%Y " + time_format)
+            return pd.to_timedelta(str(dt_time.time()))
         except (TypeError, AttributeError):
             return str(time)
 
     elif mode == 'regex':
-        if re_compiler(format) is True:
+        try:
+            re.compile(time_format)
+            is_valid = True
+        except re.error:
+            is_valid = False
+        if is_valid:
             try:
-                return pd.to_timedelta(re.search(time_format, time).group().split('.')[0])  # Removing microseconds
+                return pd.to_timedelta(re.search(time_format, time).string.split('.')[0])  # Removing microseconds
             except AttributeError:
                 return str(time)
         else:
@@ -62,15 +67,3 @@ def clean_data(datatables: list, meta: list, columns: dict, date_time: list):
     pass
 
 
-def re_compiler(format: str) -> bool:
-    """
-    :param format: raw string with regex pattern
-    :return: bool True if regex compiles, else False
-    """
-
-    try:
-        re.compile(format)
-        return True
-
-    except re.error:
-        return False
