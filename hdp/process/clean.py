dc51a7616cb5b1e2344c2264f49ac79e0ec079f8
aLo <alowy@protonmail.com>
2020-06-03 11:41:02 +0200

    Added inner functions for clean_data - set_meta_obj - rename_columns - fill_values - set_index - set_datetime - set_timedelta - set_categorical

--- hdp/process/clean.py
+++ hdp/process/clean.py
@@ -63,7 +63,125 @@ def convert_units(datatables: list):
 
 
 # Team 2
-def clean_data(datatables: list, meta: list, columns: dict, date_time: list):
-    pass
+def clean_data(datatables: list, meta: list, clean_prop: list):
+
+    def set_meta_obj(dt_list, meta_list):
+        """
+        Function to set right Metadata object as a DataTable.meta attribute
+
+        :param dt_list: List of Datatable objects
+        :type dt_list: list
+
+        :param meta_list: List of MetaData objects
+        :type meta_list: list
+        """
+        for dt, meta in zip(dt_list, meta_list):
+            dt.meta = meta
+
+    def rename_columns(dt_list, cd_list):
+        """
+        Function to rename and drop columns according to CleanData.columns attribute
+
+        :param dt_list: List of Datatable objects
+        :type dt_list: list
+
+        :param cd_list: List of CleanData objects
+        :type cd_list: list
+        """
+        for dt, cd in zip(dt_list, cd_list):
+            for col in dt.dt.columns:
+                if col not in cd.columns.keys():
+                    dt.dt = dt.dt.drop(columns=col)
+            dt.dt = dt.dt.rename(columns=cd.columns)
+
+    def fill_values(dt_list, cd_list):
+        """
+        Function to fill missing values according to CleanData.missing_data attribute
+
+        :param dt_list: List of Datatable objects
+        :type dt_list: list
+
+        :param cd_list: List of CleanData objects
+        :type cd_list: list
+        """
+        for dt, cd in zip(dt_list, cd_list):
+            for col in dt.dt.columns:
+                if not cd.missing_data[col]['permitted']:
+                    if cd.missing_data[col]['fill_value'] is not None:
+                        dt.dt[col] = dt.dt[col].fillna(value=cd.missing_data[col]['fill_value'])
+                    elif cd.missing_data[col]['fill_method'] is not None:
+                        dt.dt[col] = dt.dt[col].fillna(method=cd.missing_data[col]['fill_method'])
+                    elif cd.missing_data[col]['fill_value'] is None and cd.missing_data[col]['fill_method'] is None:
+                        dt.dt[col] = dt.dt[col].dropna()
+
+    def set_index(dt_list, meta_list):
+        """
+        Function to set index according to Metada.index attribute
+
+        :param dt_list: List of Datatable objects
+        :type dt_list: list
+
+        :param meta_list: List of MetaData objects
+        :type meta_list: list
+        """
+        for dt, meta in zip(dt_list, meta_list):
+            dt.dt = dt.dt.set_index(meta.index)
+
+    def set_datetime(dt_list, cd_list):
+        """
+        Function to set datetime values according to CleanData.date_time attribute
+
+        :param dt_list: List of Datatable objects
+        :type dt_list: list
+
+        :param cd_list: List of CleanData objects
+        :type cd_list: list
+        """
+        for dt, cd in zip(dt_list, cd_list):
+            for col in dt.dt.columns:
+                if col in cd.date_time and cd.date_time[col] == 'datetime':
+                    dt.dt[col] = dt.dt[col].apply(convert_date, args=(cd.patterns[col][0], cd.patterns[col][1]))
+                elif col in cd.date_time and col not in cd.patterns:
+                    dt.dt[col] = pd.to_datetime(dt.dt[col])
+
+    def set_timedelta(dt_list, cd_list):
+        """
+        Function to set timedelta values according to CleanData.date_time attribute
+
+        :param dt_list: List of Datatable objects
+        :type dt_list: list
+
+        :param cd_list: List of CleanData objects
+        :type cd_list: list
+        """
+        for dt, cd in zip(dt_list, cd_list):
+            for col in dt.dt.columns:
+                if col in cd.date_time and cd.date_time[col] == 'timedelta':
+                    dt.dt[col] = dt.dt[col].apply(convert_time, args=(cd.patterns[col][0], cd.patterns[col][1]))
+                elif col in cd.date_time and col not in cd.patterns:
+                    dt.dt[col] = pd.to_timedelta(dt.dt[col])
+
+    def set_categorical(dt_list, cd_list):
+        """
+        Function to change datatype according to CleanData.categorical attribute
+
+        :param dt_list: List of Datatable objects
+        :type dt_list: list
+
+        :param cd_list: List of CleanData objects
+        :type cd_list: list
+        """
+        for dt, cd in zip(dt_list, cd_list):
+            for col in dt.dt.columns:
+                if col in cd.categorical:
+                    dt.dt[col] = dt.dt[col].astype('category')
+
+    set_meta_obj(datatables, meta)
+    rename_columns(datatables, clean_prop)
+    fill_values(datatables, clean_prop)
+    set_index(datatables, meta)
+    set_datetime(datatables, clean_prop)
+    set_timedelta(datatables, clean_prop)
+    set_categorical(datatables, clean_prop)
 
 
