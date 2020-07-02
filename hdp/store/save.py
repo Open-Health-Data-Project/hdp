35c5783c187ce85c6634fafc8d82bfd752b47b64
rewaan <tomasz.gorny27@gmail.com>
2020-07-02 17:09:32 +0200

    update do testow dodane funkcje i opisy

--- hdp/store/save.py
+++ hdp/store/save.py
@@ -85,11 +85,11 @@ def _read_files(profile_name, file, mode):
     if mode == 'json':
         path = _read_directory_files(profile_name, 'json') + file + '.json'
         df_data = pd.read_json(path)
-        print(df_data, file)
+        print(df_data)
     elif mode == 'csv':
         path = _read_directory_files(profile_name, 'csv') + file + '.csv'
         df_data = pd.read_csv(path)
-        print(df_data, file)
+        print(df_data)
     elif mode == 'excel':
         path = _read_directory_files(profile_name, 'excel') + file + '.xlsx'
         df_data = pd.read_excel(path, sheet_name='data')
@@ -107,64 +107,111 @@ def _modify_time(profile_name, file, mode):
     return modify_time
 
 
-def read_data(profile_name):
+def _file_list(profile_name):
     file_set = set()
+    mode_list = ('excel', 'csv', 'json')
+    for mode in mode_list:
+        if _check_file_directory(profile_name, mode) is True:
+            for file in os.listdir(_read_directory_files(profile_name, mode)):
+                if '_data' in file:
+                    file = file.rsplit('.', 1)[0]
+                    if file not in file_set:
+                        file_set.add(file)
+    return file_set
+
+
+def _check_file_directory(profile_name, mode):
+    if os.path.isfile('{}_saved_{}_files.txt'.format(profile_name, mode)):
+        return True
+    else:
+        return False
+
+
+def _error_message(profile_name):
+    if _check_file_directory(profile_name, 'csv') is False:
+        print('No csv file directory path.')
+    if _check_file_directory(profile_name, 'json') is False:
+        print('No json file directory path.')
+    if _check_file_directory(profile_name, 'excel') is False:
+        print('No excel file directory path.')
+
+
+def read_data(profile_name):
     file_check_set = set()
     excel_modify_time = 'Sat 0'
     json_modify_time = 'Sat 0'
     csv_modify_time = 'Sat 0'
-    mode_list = ('excel', 'csv', 'json')
-    for mode in mode_list:
-        for file in os.listdir(_read_directory_files(profile_name, mode)):
-            if '_data' in file:
-                file = file.rsplit('.', 1)[0]
-                if file in file_set:
-                    if os.path.isfile(_read_directory_files(profile_name, 'excel') + file + '.xlsx')\
-                            and file not in file_check_set:
-                        excel_modify_time = _modify_time(profile_name, file, 'excel')
-                    if os.path.isfile(_read_directory_files(profile_name, 'csv') + file + '.csv')\
-                            and file not in file_check_set:
-                        csv_modify_time = _modify_time(profile_name, file, 'csv')
-                    if os.path.isfile(_read_directory_files(profile_name, 'json') + file + '.json') and \
-                            file not in file_check_set:
-                        json_modify_time = _modify_time(profile_name, file, 'json')
-                        if excel_modify_time > json_modify_time and excel_modify_time > csv_modify_time:
-                            _read_files(profile_name, file, 'excel')
-                            file_metadata_list = pd.ExcelFile(_read_directory_files(profile_name, 'excel')
-                                                              + file + '.xlsx')
-                            print(file_metadata_list.sheet_names)
-                        elif json_modify_time > excel_modify_time and json_modify_time > csv_modify_time:
-                            _read_files(profile_name, file, 'json')
-                            file_metadata_list = os.listdir(_read_directory_files(profile_name, 'json') +
-                                                            file.replace('data', 'metadata'))
-                            print(file_metadata_list)
-                        elif csv_modify_time > excel_modify_time and csv_modify_time > json_modify_time:
-                            _read_files(profile_name, file, 'csv')
-                            file_metadata_list = os.listdir(_read_directory_files(profile_name, 'csv') +
-                                                            file.replace('data', 'metadata'))
-                            print(file_metadata_list)
-                    file_check_set.add(file)
+    i = 0
+    for file in _file_list(profile_name):
+        if _check_file_directory(profile_name, 'excel') is True and os.path.isfile(_read_directory_files(profile_name, 'excel') + file + '.xlsx')\
+                and file not in file_check_set:
+            excel_modify_time = _modify_time(profile_name, file, 'excel')
+            i += 1
+        if _check_file_directory(profile_name, 'csv') is True and os.path.isfile(_read_directory_files(profile_name, 'csv') + file + '.csv')\
+                and file not in file_check_set:
+            csv_modify_time = _modify_time(profile_name, file, 'csv')
+            i += 1
+        if _check_file_directory(profile_name, 'json') is True and os.path.isfile(_read_directory_files(profile_name, 'json') + file + '.json') and\
+                file not in file_check_set:
+            json_modify_time = _modify_time(profile_name, file, 'json')
+            i += 1
+        if i >= 2:
+            if excel_modify_time > json_modify_time and excel_modify_time > csv_modify_time\
+                    and file not in file_check_set and os.path.isfile(_read_directory_files(profile_name, 'excel')
+                                                                      + file + '.xlsx'):
+                _read_files(profile_name, file, 'excel')
+                file_metadata_list = pd.ExcelFile(_read_directory_files(profile_name, 'excel')
+                                                  + file + '.xlsx')
+                print(file_metadata_list.sheet_names)
+                print('1' + file)
+                file_check_set.add(file)
+            elif json_modify_time > excel_modify_time and json_modify_time > csv_modify_time\
+                    and file not in file_check_set and os.path.isfile(_read_directory_files(profile_name, 'json')
+                                                                      + file + '.json'):
+                _read_files(profile_name, file, 'json')
+                file_metadata_list = os.listdir(_read_directory_files(profile_name, 'json') +
+                                                file.replace('data', 'metadata'))
+                print(file_metadata_list)
+                print('2' + file)
+                file_check_set.add(file)
+            elif csv_modify_time > excel_modify_time and csv_modify_time > json_modify_time\
+                    and file not in file_check_set and os.path.isfile(_read_directory_files(profile_name, 'csv')
+                                                                      + file + '.csv'):
+                _read_files(profile_name, file, 'csv')
+                file_metadata_list = os.listdir(_read_directory_files(profile_name, 'csv') +
+                                                file.replace('data', 'metadata'))
+                print(file_metadata_list)
+                print('3' + file)
+                file_check_set.add(file)
+        if file not in file_check_set:
+            if _check_file_directory(profile_name, 'excel') and _check_file_directory(profile_name, 'json') and\
+                    _check_file_directory(profile_name, 'csv') is True:
                 if not os.path.isfile(_read_directory_files(profile_name, 'excel') + file + '.xlsx') and\
                         not os.path.isfile(_read_directory_files(profile_name, 'json') + file + '.json'):
                     _read_files(profile_name, file, 'csv')
                     file_metadata_list = os.listdir(_read_directory_files(profile_name, 'csv') +
                                                     file.replace('data', 'metadata'))
                     print(file_metadata_list)
+                    print('4' + file)
+                    file_check_set.add(file)
                 if not os.path.isfile(_read_directory_files(profile_name, 'csv') + file + '.csv') and\
                         not os.path.isfile(_read_directory_files(profile_name, 'json') + file + '.json'):
                     _read_files(profile_name, file, 'excel')
                     file_metadata_list = pd.ExcelFile(_read_directory_files(profile_name, 'excel')
                                                       + file + '.xlsx')
                     print(file_metadata_list.sheet_names)
+                    print('5' + file)
+                    file_check_set.add(file)
                 if not os.path.isfile(_read_directory_files(profile_name, 'excel') + file + '.xlsx') and\
                         not os.path.isfile(_read_directory_files(profile_name, 'csv') + file + '.csv'):
                     _read_files(profile_name, file, 'json')
                     file_metadata_list = os.listdir(_read_directory_files(profile_name, 'json'))
                     print(file_metadata_list)
-                else:
-                    file_set.add(file)
+                    print('6' + file)
+                    file_check_set.add(file)
+            else:
+                _error_message(profile_name)
 
 
-save_data(pr.get_data(), 'excel')
+# save_data(pr.get_data(), 'csv')
 read_data(pr.name)
-
