38b29ce2c1336b46ef50d7ad4ac03ec505c31487
nero2310 <nero2310@wp.pl>
2020-07-03 01:24:36 +0200

    Refactoring function whose read data from tcx file
    Optimaize imports, add documentation to function

--- hdp/process/load.py
+++ hdp/process/load.py
@@ -1,9 +1,12 @@
-import pandas as pd
+from pathlib import Path
+from typing import List, Tuple, Dict
 from bs4 import BeautifulSoup
 from bs4 import NavigableString, Tag
-from typing import List,Tuple,Dict
+
+import pandas as pd
+
+
 from hdp.struct.datatable import DataTable
-from pathlib import Path
 
 
 def _get_file_name(path):
@@ -184,32 +187,57 @@ def load_gpx(gpx_files: list, params: dict = {}):
     pass
 
 
+def extract_one_field_data(trackpoint) -> Dict:
+    """
+
+    :param trackpoint:
+    :return: Not nested point values
+    """
+    point = {single_element.name: single_element.text
+             for single_element in trackpoint.children
+             if isinstance(single_element, Tag) and len(single_element.findChildren()) == 0}
+    return point
+
+
+def extract_nested_values(trackpoint) -> Dict:
+    point = {}
+    for element in trackpoint.children:
+        if isinstance(element, Tag) and len(element.findChildren()) > 1:
+            for value in element.children:
+                if isinstance(value, NavigableString):
+                    point[value.name] = value.string
+                elif isinstance(value, Tag):
+                    point[value.name] = value.text.strip()
+    return point
+
+
 # Team 3
-def load_tcx(tcx_files: List) -> Tuple[List,Dict]:
-    # def extract_trackpoints(activity):
-    #     for trackpoint in activity
+def load_tcx(tcx_files: List) -> Tuple[List, Dict]:
+    """
+    Read list of tcx files
+    :param tcx_files : List
+        List of path to tcx
+    :return:
+    DataTable list:
+        List of parsed DataTable objects with file name and DataFrame attributes filled.
+
+    Exceptions dictionary:
+        Dictionary with files names as keys and exceptions strings as values.
+        Contains only files where an exception occurred.
+    """
     exceptions_dict = {}
     data_table_list = []
-    data_table = DataTable()
-    activites = []
     for path in tcx_files:
         try:
+            data_table = DataTable()
+            activites = []
             name = _get_file_name(path)
             with (open(path, "r")) as file:
                 activity = BeautifulSoup(file, "lxml")
             for trackpoint in activity.find_all("trackpoint"):
-                point = {
-                    element.name: element.text
-                    for element in trackpoint.children
-                    if isinstance(element, Tag) and len(element.findChildren()) == 0
-                }
-                for element in trackpoint.children:
-                    if isinstance(element, Tag) and len(element.findChildren()) > 1:
-                        for value in element.children:
-                            if isinstance(value, NavigableString):
-                                point[value.name] = value.string
-                            elif isinstance(value, Tag):
-                                point[value.name] = value.text.strip()
+                point = extract_one_field_data(trackpoint)
+                nested_data = extract_nested_values(trackpoint)
+                point = {**point,**nested_data}
                 activites.append(point)
             data_table.df = pd.DataFrame(activites)
         except Exception as e:
@@ -223,4 +251,5 @@ def load_tcx(tcx_files: List) -> Tuple[List,Dict]:
 # Team 4
 def load_jpg(jpg_files: list, params: dict = {}):
     pass
+
 
